f5ca0ffa203d4d73b740695315a0a79b8b0f379e
Rithiga V <138615122+rithigavijayendran@users.noreply.github.com>
2025-02-12 22:55:36 +0530

    preparing backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"https://pet-adoption-rithi.netlify.app"}));
 
 // Routes
 app.use('/api/auth', authRoutes);
